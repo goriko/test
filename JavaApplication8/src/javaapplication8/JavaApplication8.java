aeac630299bff6b2d9caaa45cf147b6286c7007b
goriko <guanzonedrian@gmail.com>
2020-11-17 16:29:03 +0800

    Hello

--- JavaApplication8/src/javaapplication8/JavaApplication8.java
+++ JavaApplication8/src/javaapplication8/JavaApplication8.java
@@ -17,6 +17,8 @@ public class JavaApplication8 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("HELLO");
+        System.out.println("HELLO");
+        System.out.println("HELLO");
     }
     
 }
